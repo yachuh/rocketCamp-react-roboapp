191fa4826bd62ddefab94633ffa3e2c11327413d
yachuh <yachuh@gmail.com>
2022-06-08 01:59:25 +0800

    Add State

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import SearchBox from "./SearchBox";
 import { robots } from './robots';
 
 class App extends Component {
+    // Add State
+    constructor(){
+        super()
+        this.state = {
+            // state properties
+            robots: robots,
+            searchfield:''
+        }
+    }
     render(){
         return (
             <div>
                 <h1>Robo Friends</h1>
                 <SearchBox />
-                <CardList robots={robots} />
+                <CardList robots={this.state.robots} />
             </div>
         );
     }
