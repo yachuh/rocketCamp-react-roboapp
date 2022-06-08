9779a2c6da03a6ee5ed4e98fbfd77fa6a04d5394
yachuh <yachuh@gmail.com>
2022-06-08 10:31:13 +0800

    Using lifecycle methods to fetch data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from './robots';
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield:''
         }
     }
+    
     onsearchChange = (event) => {
         this.setState({ searchfield: event.target.value})
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then( response => response.json() )
+            .then( users => this.setState({robots: users }));
+    }
+    
     render(){
         const filteredRobots = this.state.robots.filter( robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
