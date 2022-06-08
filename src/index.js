e3ce71ce7c6c10fc75f59c82f490f3875163a52d
yachuh <yachuh@gmail.com>
2022-06-08 12:32:03 +0800

    Oraganizing folder structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
